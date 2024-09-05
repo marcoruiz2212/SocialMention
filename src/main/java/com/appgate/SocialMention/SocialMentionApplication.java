82927e1029bdfe6e35d2343cc45d8d2e868124a2
marcoruiz2212 <134292613+marcoruiz2212@users.noreply.github.com>
2024-09-04 21:52:57 -0500

    init project

---
+++ src/main/java/com/appgate/SocialMention/SocialMentionApplication.java
@@ -0,0 +1,13 @@
+package com.appgate.SocialMention;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMentionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialMentionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/appgate/SocialMention/SocialMentionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appgate.SocialMention;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMentionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
